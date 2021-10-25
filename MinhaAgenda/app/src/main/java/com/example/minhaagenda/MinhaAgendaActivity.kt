5662187bd20de85c79e8d374402a2e76f89f0020
Narla Chagas <ncds.fis20@uea.edu.br>
2021-10-25 11:31:22 -0400

    adiciona versão 1 agenda

---
+++ MinhaAgenda/app/src/main/java/com/example/minhaagenda/MinhaAgendaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.minhaagenda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MinhaAgendaActivity : AppCompatActivity() {
+    private lateinit var binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_minha_agenda)
+    }
+}
